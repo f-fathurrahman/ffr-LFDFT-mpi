d99c1b1935475578979d0ee30aaf904b3c39375d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-13 21:50:00 +0700

    Add option to specify whether debug or opt version of PETSc that will be used during linking

--- tests/build.sh
+++ tests/build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
   echo
-  echo "ERROR"
-  echo "Need two parameters: main file and compiler name (and or options)"
-  echo "Example: ./build.sh myfile.c \"mpicc -O2\""
+  echo "ERROR calling this dumb one-line build script"
+  echo "Need two parameters: main file, compiler command, and version (opt or debug)"
+  echo "Example:"
+  echo "./build.sh myfile.c \"mpicc -O2\" debug"
   echo
   exit 1
 fi
 
-INC="-I../src/ -I../local/petsc-3.8.3_openmpi_gnu_debug/include"
-LIB="../src/libmain.a ../local/petsc-3.8.3_openmpi_gnu_debug/lib/libpetsc.a -lblas -llapack -lfftw3 -lX11 -lm -lgfortran -ldl"
+INC="-I../src/ -I../local/petsc-3.8.3_openmpi_gnu_$3/include"
+LIB="../src/libmain.a ../local/petsc-3.8.3_openmpi_gnu_$3/lib/libpetsc.a -lblas -llapack -lfftw3 -lX11 -lm -lgfortran -ldl"
 
 bas=`basename $1 .c`
 
 # remove the previous executable
-rm -vf $bas.x
+rm -vf ${bas}_$3.x
 
-$2 $INC $1 $LIB -o $bas.x
+$2 $INC $1 $LIB -o ${bas}_$3.x
 
 echo "Test executable: $bas.x"
 
