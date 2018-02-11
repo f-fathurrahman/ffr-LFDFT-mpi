b588e2c831015fe96bd53744aaca2badaba9435c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-11 07:00:20 +0700

    Using nnzc for d_nz and o_nz

--- tests/test_add_nabla2.c
+++ tests/test_add_nabla2.c
@@ -8,7 +8,7 @@ int main( int argc, char** argv)
   petsc_start(argc, argv);
   
   LF3d_T LF3d;
-  int NN[3] = {5, 5, 5};
+  int NN[3] = {45, 45, 45};
   double AA[3] = { 0.0, 0.0, 0.0 };
   double BB[3] = { 16.0, 16.0, 16.0 };
   
@@ -40,16 +40,21 @@ int main( int argc, char** argv)
 
   PetscErrorCode ierr;
   
-  ierr = MatCreateAIJ( PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, 
-                       Npoints, Npoints, nnzc, NULL, nnzc-1, NULL, &nabla2 );
-  // need to call this ?
-  ierr = MatSetUp( nabla2 );
   
-//  if( M_MPI_my_rank == 0 ) {
+  /* FIXME: Not needed ?
+  int d_nz = nnzc/M_MPI_Nprocs; // local no. of diagonal elements
+  int o_nz = (nnzc*Npoints)/(M_MPI_Nprocs*M_MPI_Nprocs) - d_nz;
+  PetscPrintf( PETSC_COMM_WORLD, "d_nz = %d, o_nz = %d\n", d_nz, o_nz );
+  */
+
+  ierr = MatCreateAIJ( PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, 
+                       Npoints, Npoints, nnzc, NULL, nnzc, NULL, &nabla2 );
+  
+  if( M_MPI_my_rank == 0 ) {
     add_nabla2_z( Nx, Ny, Nz, LF3d.D2jl_z, &nabla2 );
     add_nabla2_y( Nx, Ny, Nz, LF3d.D2jl_y, &nabla2 );
     add_nabla2_x( Nx, Ny, Nz, LF3d.D2jl_x, &nabla2 );
-//  }
+  }
 
   ierr = MatAssemblyBegin( nabla2, MAT_FINAL_ASSEMBLY );
   ierr = MatAssemblyEnd( nabla2, MAT_FINAL_ASSEMBLY );
